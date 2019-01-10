81739d74d78f1d9bc6590c96fdba242a90a7e0c3
YuanLicc <yl123456789lxp@163.com>
2019-01-10 21:03:25 +0800

    complete schedule table

--- pcc/src/components/schedule.vue
+++ pcc/src/components/schedule.vue
@@ -8,7 +8,64 @@
     </div>
     <div>
       <div class="list" v-if="activeItem === 'list'">
+        <el-table
+          :data="scheduleList"
+          style="width: 100%">
+          <el-table-column
+            type="selection"
+            prop="content"
+            label="任务内容" class="text-center">
+          </el-table-column>
+          <el-table-column
+            prop="createUserName"
+            label="创建人"
+            width="100px" sortable>
+          </el-table-column>
+          <el-table-column
+            prop="receiverName"
+            label="接收人"
+            width="100px" sortable>
+          </el-table-column>
+          <el-table-column
+            label="创建时间"
+            width="250" sortable>
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.creatTime | formatDate }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column
+            label="截止时间"
+            width="250" sortable>
+            <template slot-scope="scope">
+              <i class="el-icon-time"></i>
+              <span style="margin-left: 10px">{{ scope.row.deadline | formatDate }}</span>
+            </template>
+          </el-table-column>
 
+          <!--<el-table-column
+            prop="isComplete"
+            label="是否完成" width="80">
+          </el-table-column>-->
+          <el-table-column
+            label="Tags"  class="text-center">
+            <template slot-scope="scope">
+              <el-tag type="success">及时完成</el-tag>
+              <el-tag type="info">超时完成</el-tag>
+              <el-tag type="danger">未完成</el-tag>
+            </template>
+          </el-table-column>
+
+          <el-table-column
+            label="操作"
+            width="200">
+            <template slot-scope="scope">
+              <el-button type="success" icon="el-icon-view" circle></el-button>
+              <el-button type="primary" icon="el-icon-edit" circle></el-button>
+              <el-button type="danger" icon="el-icon-delete" circle></el-button>
+            </template>
+          </el-table-column>
+        </el-table>
       </div>
       <div class="add" v-if="activeItem === 'add'">
         <div>
@@ -19,7 +76,8 @@
             <el-form-item label="接收人">
               <el-checkbox-group class="flex-row flex-wrap flex-start"
                                  v-model="scheduleReceivers"
-                                 v-loading="friendsLoading">
+                                 v-loading="friendsLoading"
+                                 element-loading-spinner="el-icon-loading">
                 <el-checkbox-button v-for="(item, index) in friends"
                                     :label="item.id"
                                     :key="index">
@@ -29,6 +87,7 @@
             </el-form-item>
             <el-form-item label="提醒服务">
               <el-checkbox-group v-loading="remindServiceLoading"
+                                 element-loading-spinner="el-icon-loading"
                                  class="flex-row flex-wrap flex-start"
                                  v-model="selectRemindService"
                                  >
@@ -48,8 +107,17 @@
                 :picker-options="pickerOptions">
               </el-date-picker>
             </el-form-item>
+            <el-form-item label="截止时间">
+              <el-date-picker class="flex-row flex-start"
+                              v-model="schedule.deadline"
+                              type="datetime"
+                              placeholder="选择日期时间"
+                              align="left"
+                              :picker-options="pickerOptions">
+              </el-date-picker>
+            </el-form-item>
             <el-form-item>
-              <el-button type="primary" @click="">提交</el-button>
+              <el-button type="primary" @click="addSchedule">提交</el-button>
               <el-button>取消</el-button>
             </el-form-item>
           </el-form>
@@ -60,13 +128,17 @@
 </template>
 
 <script>
+
+  import DateUtil from '../util/DateUtil'
+
   export default {
     name: "schedule",
     data: function () {
       return {
-        activeItem:'add',
+        scheduleList:[],
+        activeItem:'list',
         schedule: {
-
+          pccUserId: this.$store.getters.userInfo.id
         },
         scheduleReceivers:[],
         remindServices:[],
@@ -99,13 +171,76 @@
       }
     },
     mounted: function() {
-      this.getFriends()
-      this.getRemindService()
+      this.relationList()
+    },
+    filters: {
+      formatDate: function(dateStr) {
+
+        if(!dateStr) {
+          return ''
+        }
+
+        const date = new Date(dateStr)
+
+        return DateUtil.formatDate(date, 'yyyy-MM-dd hh:mm:ss')
+      }
     },
     methods: {
+      relationList: function() {
+        this.$store.commit('showLoading')
+
+        // 请求
+        this.$axios.get('/pcc/schedule/relation', {
+          params: {
+            page:1,
+            size: 20,
+            pccUserId: this.$store.getters.userInfo.id
+          }
+        })
+          .then(res => {
+            const data = res.data
+
+            if(data.data == null) {
+              // 弹框
+            }
+            else {
+              this.scheduleList = data.data.list
+            }
+
+            console.log(res.data)
+            this.$store.commit('hideLoading')
+          })
+          .catch(err => {
+            this.$store.commit('hideLoading')
+          })
+
+      },
       addSchedule: function() {
         this.$store.commit('showLoading')
 
+        let data = {
+          pccSchedule: this.schedule,
+          scheduleReceivers: this.scheduleReceivers,
+          remindServices: this.selectRemindService
+        }
+
+        // 请求
+        this.$axios.post('/pcc/schedule/add', data)
+          .then(res => {
+            const data = res.data
+
+            if(data.data == null) {
+              // 弹框
+            }
+            else {
+            }
+
+            console.log(res.data)
+            this.$store.commit('hideLoading')
+          })
+          .catch(err => {
+            this.$store.commit('hideLoading')
+          })
 
       },
       getRemindService: function() {
@@ -136,6 +271,13 @@
       },
       exchange: function (item) {
         this.activeItem = item
+        if(item === 'list') {
+          this.relationList()
+        }
+        else if(item === 'add') {
+          this.getFriends()
+          this.getRemindService()
+        }
       },
       getFriends: function () {
         this.friendsLoading = true
--- pcc/src/util/DateUtil.js
+++ pcc/src/util/DateUtil.js
@@ -62,6 +62,28 @@ const dateUtil = {
     tempDate.setDate(date.getDate() - num)
 
     return tempDate;
+  },
+  formatDate: function (date, fmt) {
+    if (/(y+)/.test(fmt)) {
+      fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+    }
+    let o = {
+      'M+': date.getMonth() + 1,
+      'd+': date.getDate(),
+      'h+': date.getHours(),
+      'm+': date.getMinutes(),
+      's+': date.getSeconds()
+    }
+    for (let k in o) {
+      if (new RegExp(`(${k})`).test(fmt)) {
+        let str = o[k] + ''
+        fmt = fmt.replace(RegExp.$1, RegExp.$1.length === 1 ? str : dateUtil.padLeftZero(str))
+      }
+    }
+    return fmt
+  },
+  padLeftZero (str) {
+    return ('00' + str).substr(str.length)
   }
 }
 
