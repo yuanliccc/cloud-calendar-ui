5408bc1b9496a116170fd6826a8ac20c2f4b2bb6
袁李 <yl123456789lxp@gmail.com>
2019-05-30 19:33:23 +0800

    new

--- pcc/src/components/chat.vue
+++ pcc/src/components/chat.vue
@@ -2,13 +2,29 @@
   <div>
     <el-dialog width="850px" :visible.sync="isShow" :modal="true" :close-on-click-modal="false" :show-close="false">
       <div class="title flex-row flex-start" slot="title">
+        <div class="flex-column flex-start">
+          聊天信息
+        </div>
         <div class="flex-grow flex-row flex-end">
           <div class="el-icon-close pointer" @click="closeDialog"></div>
         </div>
       </div>
+      <div class="flex-row flex-center chat-content">
+        <div class="chat-left">
+          <div class="friends-item flex-row flex-center" v-for="(friend, index) in friends" :key="index">
+            <div class="friends-item-block flex-row">
+              <div class="flex-start flex-column flex-center">
+                <el-button type="primary" class="friends-item-icon" circle>{{friend.name.substring(0, 1)}}</el-button>
+              </div>
+              <div class="flex-end flex-column flex-center friends-item-name">
+                {{friend.name}}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="chat-right">
 
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="closeDialog">关闭</el-button>
+        </div>
       </div>
     </el-dialog>
   </div>
@@ -19,20 +35,24 @@
     data() {
       return {
         websock: null,
+        friends: [],
       }
     },
     props: {
       isShow: Boolean
     },
     created() {
-      //this.initWebSocket();
+      this.initWebSocket();
+    },
+    mounted: function() {
+      this.getFriends()
     },
     destroyed() {
       this.websock.close() //离开路由之后断开websocket连接
     },
     methods: {
       initWebSocket() { //初始化weosocket
-        const wsUri = "ws://127.0.0.1:8090/pcc/chat/" + this.$store.getters.userInfo.id;
+        const wsUri = "ws://127.0.0.1:8090/chat/" + this.$store.getters.userInfo.id;
         this.websock = new WebSocket(wsUri);
         this.websock.onmessage = this.websocketonmessage;
         this.websock.onopen = this.websocketonopen;
@@ -57,15 +77,79 @@
       },
       closeDialog: function () {
         this.$emit('close');
+      },
+      getFriends: function () {
+        // 请求
+        this.$axios.get('/pcc/user/friends', {
+          params: {
+            pccUserId: this.$store.getters.userInfo.id
+          }
+        })
+        .then(res => {
+          const data = res.data
+          this.friends = data.data
+        })
+        .catch(err => {
+          console.log(err)
+        })
       }
-    },
-  }</script>
+    }
+  }
+</script>
 <style scoped>
+
+  .friends-item-icon {
+    width: 38px;
+    height: 38px;
+    font-size: 15px;
+  }
+
+  .friends-item-name {
+    margin-left: 10px;
+  }
+
+  .friends-item-block {
+    width: 96%;
+  }
+
+  .friends-item {
+    height: 45px;
+    border-bottom: 1px solid rgba(96, 98, 102, 0.31);
+    box-shadow: 0 1px 2px 0 rgba(0, 0, 0, .1);
+    cursor: pointer;
+  }
+
+  .friends-item-active {
+    background: #333;
+    color: white;
+  }
+
+  .friends-item:hover {
+    background: #333;
+    color: white;
+  }
+
+  .chat-right {
+    width: 650px;
+    height: 450px;
+  }
+
+  .chat-content {
+    width: 100%;
+    height: 450px;
+    border: 1px solid #c6cdd4;
+  }
+
+  .chat-left {
+    width: 200px;
+    border-right:1px solid red;
+    overflow-y: scroll;
+  }
   .el-dialog__header {
     padding: 0 !important;
   }
 
   .el-dialog__body {
-    padding: 0px;
+    padding: 0px!important;
   }
 </style>
